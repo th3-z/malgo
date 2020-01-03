84dd9693a1ecbfab8924acbaff8614e2d7246b1f
the_z <the_z@znel.org>
2020-01-03 16:17:01 +0000

    Additional queries

--- storage/queries.go
+++ storage/queries.go
@@ -17,11 +17,34 @@ func AddUser(db Queryer, user *path.User) int64 {
 
 func AddAnime(db Queryer, anime *path.Anime) int64 {
 	query := `
-        INSERT INTO series (name)
-        VALUES (?)
+        INSERT INTO series (
+            name,
+            animedb_id,
+            series_type_id,
+            series_status_id
+        ) VALUES (
+            ?,
+            ?,
+            1,
+            1
+        )
     `
 
-	insert_id := PreparedQuery(db, query, anime.SeriesTitle)
+	insert_id := PreparedQuery(db, query, anime.SeriesTitle, anime.SeriesAnimeDbId)
 	return insert_id
 }
 
+func AddUserAnime(db Queryer, userId int, seriesId int) int64 {
+	query := `
+        INSERT INTO user_series (
+            user_id,
+            series_id
+        ) VALUES (
+            ?,
+            ?
+        )
+    `
+
+	insert_id := PreparedQuery(db, query, userId, seriesId)
+	return insert_id
+}
